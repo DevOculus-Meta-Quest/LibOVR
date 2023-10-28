e83d7618188176898464ce3de1dd3ea2ff870479
Eliminater74 <eliminater74@gmail.com>
2023-10-28 10:23:06 -0400

    Updated: small Updated Changes

--- src/LibOVR.Native/LibOVRNative.cpp
+++ src/LibOVR.Native/LibOVRNative.cpp
@@ -1,8 +1,16 @@
 
 
 #include "OVR_CAPI.h"
+#include "OVR_CAPI_Audio.h"
 #include "OVR_CAPI_D3D.h"
+#include "OVR_CAPI_GL.h"
+#include "OVR_CAPI_Keys.h"
+#include "OVR_CAPI_Vk.h"
+#include "OVR_ErrorCode.h"
+#include "OVR_Version.h"
+#include "Extras/OVR_CAPI_Util.h"
 #include "Extras/OVR_Math.h"
+#include "Extras/OVR_StereoProjection.h"
 #include "d3d11.h"
 #include "vector"
 
